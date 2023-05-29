5cbca2abe14f04a4b7a7a7fb348cf40eba074602
scotty <scottadamr01@gmail.com>
2023-05-28 21:09:10 -0400

    implement slider select

--- src/App.js
+++ src/App.js
@@ -9,11 +9,23 @@ import TenureSelect from "./Components/TenureSelect";
 function App() {
   return (
     <div className="App">
+        const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  )
+
+  <div className="App">
   <Navbar />
   <Container maxWidth="xl" sx={{marginTop:4}}>
     <Grid container spacing={5} alignItems="center">
       <Grid item xs={12} md={6}>
-        <SliderSelect />
+
+        {/* this is where we write the code  👇 */}
+        <SliderSelect data={data} setData={setData}/>
+
         <TenureSelect />
       </Grid>
       <Grid item xs={12} md={6}>
@@ -22,7 +34,5 @@ function App() {
     </Grid>
   </Container>
 </div>
-  );
-}
 
 export default App;
--- src/Components/SliderSelect.js
+++ src/Components/SliderSelect.js
@@ -1,1 +1,13 @@
+import React from "react";
+import SliderComponent from "./Common/SliderComponent";
 
+const SliderSelect = ({ data, setData }) => {
+  const bank_limit = 10000;
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default SliderSelect;

8428eecdf7c737af5ba71dce35e416c879f6a3b0
scotty <scottadamr01@gmail.com>
2023-05-28 21:24:49 -0400

    fix app.js errors

--- src/App.js
+++ src/App.js
@@ -7,32 +7,31 @@ import SliderSelect from "./Components/SliderSelect";
 import TenureSelect from "./Components/TenureSelect";
 
 function App() {
-  return (
-    <div className="App">
-        const [data, setData] = useState({
-    homeValue: 3000,
-    downPayment: 3000 * 0.2,
-    loanAmount: 3000 * 0.8,
-    loanTerm: 5,
-    interestRate: 5,
-  )
-
-  <div className="App">
-  <Navbar />
-  <Container maxWidth="xl" sx={{marginTop:4}}>
-    <Grid container spacing={5} alignItems="center">
-      <Grid item xs={12} md={6}>
-
-        
-        <SliderSelect data={data} setData={setData}/>
-
-        <TenureSelect />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Result/>
-      </Grid>
-    </Grid>
-  </Container>
-</div>
+    const [data, setData] = useState({
+      homeValue: 3000,
+      downPayment: 3000 * 0.2,
+      loanAmount: 3000 * 0.8,
+      loanTerm: 5,
+      interestRate: 5,
+      // monthlyPayment: 0,
+    });
+  
+    return (
+      <div className="App">
+        <Navbar />
+        <Container maxWidth="xl" sx={{marginTop:4}}>
+          <Grid container spacing={5} alignItems="center">
+            <Grid item xs={12} md={6}>
+              <SliderSelect data={data} setData={setData} />
+              <TenureSelect data={data} setData={setData}/>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Result data={data}/>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
 
 export default App;
